11401988d2d848e7ce0c6d25fe8aab80b1b58924
AtesComp <atescomp@gmail.com>
2023-10-17 01:00:13 -0400

    Update Git Ignore files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/keven/Documents/Dev/Workspace/caps
-# Build directory: /home/keven/Documents/Dev/Workspace/caps/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
